e4fa378223fb5388ed73ebc40408baf31fef813c
Daniel Erceg <s8daerce@stud.uni-saarland.de>
2024-12-04 20:18:32 +0100

    Minor minor changes, but now up to date with the newest version of ableton

--- MackieControl/MackieControlComponent.py
+++ MackieControl/MackieControlComponent.py
@@ -1,48 +1,43 @@
-# uncompyle6 version 3.9.1
-# Python bytecode version base 3.7.0 (3394)
-# Decompiled from: Python 3.12.2 (main, Feb  6 2024, 20:19:44) [Clang 15.0.0 (clang-1500.1.0.2.5)]
-# Embedded file name: output/Live/mac_universal_64_static/Release/python-bundle/MIDI Remote Scripts/MackieControl/MackieControlComponent.py
-# Compiled at: 2024-03-09 01:30:22
-# Size of source mod 2**32: 2500 bytes
+# Decompiled with PyLingual (https://pylingual.io)
+# Internal filename: ..\..\..\output\Live\win_64_static\Release\python-bundle\MIDI Remote Scripts\MackieControl\MackieControlComponent.py
+# Bytecode version: 3.11a7e (3495)
+# Source timestamp: 2024-09-26 15:27:47 UTC (1727364467)
 from __future__ import absolute_import, print_function, unicode_literals
-from builtins import object
-import Live
 from .consts import *
 
 class MackieControlComponent(object):
-
     def __init__(self, main_script):
-        self._MackieControlComponent__main_script = main_script
+        self.__main_script = main_script
 
     def destroy(self):
-        self._MackieControlComponent__main_script = None
+        self.__main_script = None
 
     def main_script(self):
-        return self._MackieControlComponent__main_script
+        return self.__main_script
 
     def shift_is_pressed(self):
-        return self._MackieControlComponent__main_script.shift_is_pressed()
+        return self.__main_script.shift_is_pressed()
 
     def option_is_pressed(self):
-        return self._MackieControlComponent__main_script.option_is_pressed()
+        return self.__main_script.option_is_pressed()
 
     def control_is_pressed(self):
-        return self._MackieControlComponent__main_script.control_is_pressed()
+        return self.__main_script.control_is_pressed()
 
     def alt_is_pressed(self):
-        return self._MackieControlComponent__main_script.alt_is_pressed()
+        return self.__main_script.alt_is_pressed()
 
     def song(self):
-        return self._MackieControlComponent__main_script.song()
+        return self.__main_script.song()
 
     def script_handle(self):
-        return self._MackieControlComponent__main_script.handle()
+        return self.__main_script.handle()
 
     def application(self):
-        return self._MackieControlComponent__main_script.application()
+        return self.__main_script.application()
 
     def send_midi(self, bytes):
-        self._MackieControlComponent__main_script.send_midi(bytes)
+        self.__main_script.send_midi(bytes)
 
     def request_rebuild_midi_map(self):
-        self._MackieControlComponent__main_script.request_rebuild_midi_map()
+        self.__main_script.request_rebuild_midi_map()
--- MackieControl/__init__.py
+++ MackieControl/__init__.py
@@ -1,11 +1,10 @@
-# uncompyle6 version 3.9.1
-# Python bytecode version base 3.7.0 (3394)
-# Decompiled from: Python 3.12.2 (main, Feb  6 2024, 20:19:44) [Clang 15.0.0 (clang-1500.1.0.2.5)]
-# Embedded file name: output/Live/mac_universal_64_static/Release/python-bundle/MIDI Remote Scripts/MackieControl/__init__.py
-# Compiled at: 2024-03-09 01:30:22
-# Size of source mod 2**32: 1277 bytes
-from __future__ import absolute_import, print_function, unicode_literals
+# Decompiled with PyLingual (https://pylingual.io)
+# Internal filename: ..\..\..\output\Live\win_64_static\Release\python-bundle\MIDI Remote Scripts\MackieControl\__init__.py
+# Bytecode version: 3.11a7e (3495)
+# Source timestamp: 2024-09-26 15:27:47 UTC (1727364467)
+
 from .MackieControl import MackieControl
+
 
 def create_instance(c_instance):
     return MackieControl(c_instance)
@@ -13,17 +12,11 @@ def create_instance(c_instance):
 
 from _Framework.Capabilities import *
 
+
 def get_capabilities():
-    return {CONTROLLER_ID_KEY: (controller_id(vendor_id=2675,
-                          product_ids=[6],
-                          model_name="MCU Pro USB v3.1")), 
-     
-     PORTS_KEY: [
-                 inport(props=[SCRIPT, REMOTE]),
-                 inport(props=[]),
-                 inport(props=[]),
-                 inport(props=[]),
-                 outport(props=[SCRIPT, REMOTE]),
-                 outport(props=[]),
-                 outport(props=[]),
-                 outport(props=[])]}
+    return {CONTROLLER_ID_KEY: controller_id(vendor_id=2675, product_ids=[6],
+                                             model_name='MCU Pro USB v3.1'),
+            PORTS_KEY: [inport(props=[SCRIPT, REMOTE]), inport(props=[]),
+                        inport(props=[]), inport(props=[]),
+                        outport(props=[SCRIPT, REMOTE]), outport(props=[]),
+                        outport(props=[]), outport(props=[])]}
